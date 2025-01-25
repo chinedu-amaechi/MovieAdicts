81f85f3366a6dfed277480403b1dd76f65e5506f
Chinedu Amaechi <chymez56@yahoo.com>
2025-01-25 15:53:36 -0700

    Applying Context

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,18 +1,21 @@
-import "../css/Favorites.css"
-import MovieCard from "../components/MovieCard"
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
-  const { favorites } = useMovieContext()
+  const { favorites } = useMovieContext();
 
   if (favorites) {
     return (
       <div className="favorites">
         <h2>Your Favorites</h2>
         <div className="movies-grid">
-          
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
         </div>
       </div>
-    )
+    );
   }
 
   return (
@@ -20,7 +23,7 @@ function Favorites() {
       <h2>No Favorite Movies Yet</h2>
       <p>Start adding movies to your favorites and they will appear here!</p>
     </div>
-  )
+  );
 }
 
-export default Favorites
+export default Favorites;
